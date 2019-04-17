6f40c95dfd31af6a33b8e607b5afd613beea8cfa
Z <asterixandisaac@gmail.com>
2019-04-17 10:15:23 -0600

    last commit

--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -1,19 +1,31 @@
 <template>
-  <div class="about" v-if="user">
+  <div class="leaderboard">
     <h1>Leaderboard</h1>
-    <div v-for="game in games" :key="game._id">
-      <p>{{ game.name}} </p>
-      <p>{{ game.turns }}</p>
-      <p>{{ game.duration }}</p>
-      <p>{{ game.date }}</p>
+    <div class="scores">
+      <div v-for="game in allGames" :key="game._id">
+        <p class="label">Name: <span class="notBold">{{ game.name }}</span></p> 
+        <p class="label">Turns: <span class="notBold">{{ game.turns }}</span></p> 
+        <p class="label">Duration: <span class="notBold">{{ game.duration }}</span></p> 
+        <p class="label">Date: <span class="notBold">{{ formatDate(game.created) }}</span></p> 
+      </div>
     </div>
-  </div>
-  <div v-else-if="!user">
-    <p>Please log in to see your games</p>
+    <div class="scores" v-if="user">
+      <h1>Your Last 5 Games</h1>
+      <div v-for="game in personalGames" :key="game._id">
+        <p class="label">Turns: <span class="notBold">{{ game.turns }}</span></p> 
+        <p class="label">Duration: <span class="notBold">{{ game.duration }}</span></p> 
+        <p class="label">Date: <span class="notBold">{{ formatDate(game.created) }}</span></p> 
+      </div>
+    </div>
+    <div v-else>
+      <h1>Log in to see your own scores.</h1>
+    </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
   name: 'leaderboard',
   data() {
@@ -23,7 +35,23 @@ export default {
     }
   },
   methods: {
-
+    userGames() {
+      this.personalGames = this.games
+        .filter(game => game.user == this.user._id)
+        .sort((a,b) => a.duration - b.duration)
+        .slice(0, 5);
+    },
+    topGames() {
+      this.allGames = this.games
+        .sort((a,b) => a.duration - b.duration)
+        .slice(0, 5);
+    },
+    formatDate(date) {
+      if (moment(date).diff(Date.now(), 'days') < 15)
+        return moment(date).fromNow();
+      else
+        return moment(date).format('d MMMM YYYY');
+    },
   },
   computed: {
     games: function() {
@@ -36,11 +64,29 @@ export default {
   async created() {
     await this.$store.dispatch("getUser");
     await this.$store.dispatch("getGames");
+    this.userGames();
+    this.topGames();
   }
 }
 </script>
 
 <style scoped>
+
+.scores {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  text-align: left;
+}
+
+.label {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.notBold {
+  font-weight: normal;
+}
 
 </style>
 
